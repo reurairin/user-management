469c8e2f93823d0cc95288e3f57935fd816730fa
Danil Makushev <reurairin@gmail.com>
2025-02-03 00:27:17 +0200

    add primeng

--- client/src/app/pages/users-page/users-page.component.ts
+++ client/src/app/pages/users-page/users-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserEditFormComponent } from '../../entities/user-edit-form/user-edit-form.component';
 import { UserOverviewComponent } from '../../entities/user-overview/user-overview.component';
+import { NavbarComponent } from '../../shared/ui/navbar/navbar.component';
 
 @Component({
   selector: 'app-users-page',
-  imports: [UserEditFormComponent, UserOverviewComponent],
+  imports: [NavbarComponent, UserEditFormComponent, UserOverviewComponent],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.css',
 })
---
+++ client/src/app/shared/ui/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [ToolbarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
